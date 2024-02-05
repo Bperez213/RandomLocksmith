538e065573b6fb2dcd79f8ddf6c5070969b266c0
Bperez <bjp236@gmail.com>
2024-02-05 13:50:30 -0500

    Added query selectors for JS file

---
+++ main.js
@@ -0,0 +1,5 @@
+const passwordInput = document.querySelector(".passwordBox input"),
+	copyIcon = document.querySelector(".passwordBox .copyIcon"),
+	rangeInput = document.querySelector(".lengthBox input"),
+	sliderNumber = document.querySelector(".rangeBox .sliderNumber"),
+	genPasswordButton = document.querySelector(".generatePassword");
